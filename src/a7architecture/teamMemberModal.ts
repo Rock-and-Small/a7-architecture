a56894f6f91fc59b92495f9f06965adcdcd59614
Digerati <cabal@digerati.design>
2025-02-03 18:40:42 +0000

    Conditionally display team member modal email link

--- src/a7architecture/teamMemberModal.ts
+++ src/a7architecture/teamMemberModal.ts
@@ -23,10 +23,17 @@ export const teamMemberModal = () => {
             modal.querySelector(['[dd-modal="qualification"]']).innerText = teamMember.querySelector('[dd-tm="qualification"]').innerText;
             modal.querySelector(['[dd-modal="job-title"]']).innerText = teamMember.querySelector('[dd-tm="job-title"').innerText;
             modal.querySelector(['[dd-modal="image"]']).src = teamMember.querySelector('[dd-tm="image"]').src;
-            modal.querySelector(['[dd-modal="email"]']).href = teamMember.querySelector('[dd-tm="email"]').href;
             modal.querySelector(['[dd-modal="biography"]']).innerHTML = teamMember.querySelector('[dd-tm="biography"]').innerHTML;
             modal.querySelector(['[dd-modal="fact-question"]']).innerHTML = teamMember.querySelector('[dd-tm="fact-question"]').innerHTML;
             modal.querySelector(['[dd-modal="fact-answer"]']).innerHTML = teamMember.querySelector('[dd-tm="fact-answer"]').innerHTML;
+            /* Display email */
+            let emailAddress = teamMember.querySelector('[dd-tm="email"]').href.trim();
+            let emailElement = modal.querySelector(['[dd-modal="email"]']);
+            if (!emailAddress.includes('#')) {
+                emailElement?.classList.remove('hide');
+            } else {
+                emailElement?.classList.add('hide');
+            }
         }, { passive: true });
     });
 };
